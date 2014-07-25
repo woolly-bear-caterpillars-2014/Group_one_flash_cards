718c69562c4308665a3531cb36257f5497cc1dca
& <pair+abcm1989.eddiekimosabe@devbootcamp.com>
2014-07-25 14:45:32 -0400

    New

--- flashcards.rb
+++ flashcards.rb
@@ -1,5 +1,6 @@
 
-require "pry"
+# require "pry"
+require "prettyprint"
 
 module DeckBuilder
 
@@ -10,14 +11,14 @@ module DeckBuilder
 
 
   def self.parse_data(raw_card_data)
-  definitions = []
-  answers = []
-  raw_card_data.reject! {|line| line.empty? }
-  raw_card_data.each_with_index do |line, i|
-    definitions << line if i % 2 == 0
-    answers << line if i % 2 != 0
+    definitions = []
+    answers = []
+    raw_card_data.reject! {|line| line.empty? }
+    raw_card_data.each_with_index do |line, i|
+      definitions << line if i % 2 == 0
+      answers << line if i % 2 != 0
   end
-  data = definitions.zip(answers)
+    data = definitions.zip(answers)
   end
 
   def self.build_deck(filename)
@@ -28,7 +29,7 @@ module DeckBuilder
 end
 
 class Deck
-  attr_reader :cards
+  attr_accessor :cards
 
   def initialize(cards)
     @cards = cards
@@ -54,25 +55,35 @@ end
 
 class Controller
 attr_accessor :card, :current_session, :view
+  
   def initialize 
     @view = View.new
     run
   end
+  
   def run
-    
-    until view.deck.cards.length==0 
+
+    until view.deck.cards.length==0
     view.show_card
     view.display_prompt 
     count=0
-      until view.get_input==view.current_card.answer || count==3
+    answer = view.get_input
+      until answer==view.current_card.answer || count==2
+        view.deck.cards = [] if answer == "quit"
+        break if answer == "quit"
           view.wrong_answer
           count+=1
-      end
+          answer = view.get_input
+          end
       
-    unless count==3 
-      view.right_answer
+    if count==2
+      view.show_answer
+    else
+      view.right_answer unless answer == "quit"
     end
+    
     end
+
     view.game_over
   end
 end
@@ -81,6 +92,7 @@ class View
   attr_accessor :deck, :current_card
   WRONGANSWERS=["I don't have the time or the crayons to explain it to you.", "Did your parents ever ask you to run away from home?", "If you were any more stupid, you'd have to be watered once a week.", "A demitasse would fit your head like a sombrero.", "You have one brain cell and it's fighting for domination.", "I had a nightmare. I dreamt I was you.", "I know you're nobody's fool, but maybe someone will adopt you.", "Ordinary people live and learn. You just live.", "If what you know can't hurt you, you're practically invincible.", "Keep talking, someday you'll say something intelligent.", "You're a habit I'd like to kick with both feet.", "You're like one of those idiot savants, except without the 'savant' part.", "You're the best at all you do! And all you do is make people hate you.", "You remind me of the ocean. You make me sick.", "A halfwit gave you a piece of his mind and you held onto it.", "Don't let your mind wander. It's way too small to be outside by itself.", "Earth is full. Go home.", "I bet your brain feels as good as new seeing as you've never used it.", "Your brain waves fall a little short of the beach.", "That's it. No more free will.", "If I throw a stick, will you leave?"]
 
+  INSULTS=["Moron", "Dimwit", "Dingbat", "Imbecile", "Halfwit", "Dullard", "Dolt", "Twit", "Weakling"]
   
 
   def initialize
@@ -90,7 +102,7 @@ class View
   end
 
   def welcome_to_flashcards
-    puts "Welcome!"
+    puts "Welcome. Type quit to exit at anytime."
   end
 
   def get_input
@@ -98,6 +110,7 @@ class View
   end
 
   def display_prompt
+
     puts "What word corresponds with this definition?"
   end
 
@@ -105,6 +118,10 @@ class View
     @current_card=deck.cards.sample
     deck.cards.delete_if { |card| card==current_card}
     puts current_card.definition
+  end
+
+  def show_answer
+    puts INSULTS.sample + "." " The correct answer is " + current_card.answer + "."
   end
 
   def wrong_answer
@@ -116,8 +133,9 @@ class View
   end
 
   def game_over
-    puts "Game over! Do you want to play again?"
+    puts "Thank god. Nobody wants you to play again."
   end
 end 
 
-binding.pry
+Controller.new
+# binding.pry
